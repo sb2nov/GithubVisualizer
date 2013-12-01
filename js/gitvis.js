6b54eacf63801dfeda7a4434174b5db54973aaaf
Sourabh <sb2nov@gmail.com>
2013-12-01 14:58:45 -0500

    two trees

--- js/gitvis.js
+++ js/gitvis.js
@@ -1,4 +1,6 @@
 var mouseoverTimer;
+var repoSelected = null;
+var userSelected = null;
 
 function init(){
     parsetimestamp = d3.time.format("%Y-%m-%dT%H:%M:%SZ").parse;
@@ -9,33 +11,33 @@ function init(){
         width = windowwidth - margin.left - margin.right,
         height = windowheight - margin.top - margin.bottom;
         
-    div = d3.select("#timeline-div").append("div")
+    timeDiv = d3.select("#timeline-div").append("div")
         .style("position", "relative")
         .style("width", (width + margin.left + margin.right) + "px")
         .style("height", (height + margin.top + margin.bottom) * 0.23 + "px")
         .style("left", margin.left + "px")
         .style("top", margin.top + "px");
 
-    div = d3.select("#treemap-repo-div").append("div")
+    repoMapDiv = d3.select("#treemap-repo-div").append("div")
         .style("position", "relative")
         .style("width", ((width + margin.left + margin.right)*0.4) + "px")
         .style("height", ((height + margin.top + margin.bottom)*0.5) + "px")
-        .style("left", margin.left + "px")
-        .style("top", (margin.top + (height + margin.top + margin.bottom) * 0.25) + "px");
+        // .style("left", margin.left + "px")
+        .style("top", margin.top + "px");
 
-    div = d3.select("#treemap-user-div").append("div")
+    userMapDiv = d3.select("#treemap-user-div").append("div")
         .style("position", "relative")
-        .style("width", (width + margin.left + margin.right)*0.4 + "px")
-        .style("height", ((height + margin.top + margin.bottom)*0.5) + "px")
-        .style("left", ((windowwidth/2.0) + margin.left) + "px")
-        .style("top", (margin.top + (height + margin.top + margin.bottom) * 0.25) + "px");
+        .style("width", width*0.4 + "px")
+        .style("height", (height*0.5) + "px")
+        .style("left", margin.left+ 150 + "px")
+        .style("top", margin.top + "px");
 
-    div = d3.select("#heatmap-div").append("div")
+    heatDiv = d3.select("#heatmap-div").append("div")
         .style("position", "relative")
         .style("width", (width + margin.left + margin.right) + "px")
         .style("height", (height + margin.top + margin.bottom) * 0.23 + "px")
         .style("left", margin.left + "px")
-        .style("top", (margin.top + (height + margin.top + margin.bottom) * 0.75) + "px");
+        .style("top", margin.top + "px");
 
 
     tooltip_div = d3.select("body").append("div")   
@@ -60,13 +62,21 @@ function init(){
 
     color = d3.scale.category20()
 
-    // treemap = d3.layout.treemap()
-    //     .size([width, height])
-    //     .sticky(true)
-    //     .children(function(d) { return d.values; })
-    //     .value(function(d) { return d.values.total; })
-    //     // .sort(function(a,b) {return a.total - b.total})
-    //     .round(false);
+    repoTreeMap = d3.layout.treemap()
+        .size([(width + margin.left + margin.right)*0.5, (height + margin.top + margin.bottom)*0.5])
+        .sticky(true)
+        .children(function(d) { return d.values; })
+        .value(function(d) { return d.values.total; })
+        // .sort(function(a,b) {return a.total - b.total})
+        .round(false);
+
+    userTreeMap = d3.layout.treemap()
+        .size([(width)*0.5, (height)*0.5])
+        .sticky(true)
+        .children(function(d) { return d.values; })
+        .value(function(d) { return d.values.total; })
+        // .sort(function(a,b) {return a.total - b.total})
+        .round(false);
 
     d3.select('#select-score').property('checked', true);
 
@@ -100,6 +110,16 @@ function formatData(error, csv) {
       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
       .entries(csv);
 
+    repoData = d3.nest()
+      .key(function(d) {return d.repo;})
+      .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
+      .entries(csv);
+
+    userData = d3.nest()
+      .key(function(d) {return d.username;})
+      .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
+      .entries(csv);
+
     repoTimeData = d3.nest()
       .key(function(d) {return d.repo;})
       .key(function(d) {return d.timestamp;})
@@ -110,6 +130,9 @@ function formatData(error, csv) {
       })
       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
       .entries(csv);
+
+    repoTimeDataObj = new Object();
+    repoTimeData.forEach(function(d) {repoTimeDataObj[d.key] = d.values;});
 
     userTimeData = d3.nest()
       .key(function(d) {return d.username;})
@@ -122,6 +145,8 @@ function formatData(error, csv) {
       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
       .entries(csv);
 
+    userTimeDataObj = new Object();
+    userTimeData.forEach(function(d) {userTimeDataObj[d.key] = d.values;});
 
     userRepoData = d3.nest()
       .key(function(d) {return d.username;})
@@ -129,11 +154,17 @@ function formatData(error, csv) {
       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
       .entries(csv);
 
+    userRepoDataObj = new Object();
+    userRepoData.forEach(function(d) {userRepoDataObj[d.key] = d.values;});
+
     repoUserData = d3.nest()
       .key(function(d) {return d.repo;})
       .key(function(d) {return d.username;})
       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
       .entries(csv);
+
+    repoUserDataObj = new Object();
+    repoUserData.forEach(function(d) {repoUserDataObj[d.key] = d.values;});
       
     repoUserTimeData = d3.nest()
       .key(function(d) {return d.repo;})
@@ -147,6 +178,13 @@ function formatData(error, csv) {
       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
       .entries(csv);
 
+    repoUserTimeDataObj = new Object();
+    repoUserTimeData.forEach(function(d) {
+      tempObj = new Object();
+      d.values.forEach(function(d) { tempObj[d.key] = d.values;})
+      repoUserTimeDataObj[d.key] = tempObj;}
+    );
+
     userRepoTimeData = d3.nest()
       .key(function(d) {return d.username;})
       .key(function(d) {return d.repo;})
@@ -159,14 +197,128 @@ function formatData(error, csv) {
       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
       .entries(csv);
 
+    userRepoTimeDataObj = new Object();
+    userRepoTimeData.forEach(function(d) {
+      tempObj = new Object();
+      d.values.forEach(function(d) { tempObj[d.key] = d.values;})
+      userRepoTimeDataObj[d.key] = tempObj;}
+    );
+
       // Main Call
       draw();
 
 }
 
-
 function draw(){
-    reponame = "balanced-python";
+  drawTreeMap();
+}
+
+
+function drawTreeMap(){
+
+  var nodelinkRepo = repoMapDiv.datum({values: repoData, key:'repo_treemap'}).selectAll(".nodelink")
+      .data(repoTreeMap.nodes)
+    .enter().append("a")
+      .attr("class", "nodelink")
+      // .attr('href',function(d)  {return d.values.total ? d.key : null; })
+
+  var nodeRepo = nodelinkRepo
+    .append("div")
+      .attr("class", "node")
+      .call(position)
+      .attr("stroke", "#FFF")
+      .style("background", function(d) { return d.values ? d3.rgb(color(d.key)) : null; })
+           .on("mouseover", function(d){
+                tooltip_div       
+                    .style("visibility", "visible")
+                    .transition().duration(150)
+                    .style("opacity", 1);
+                  var putImageInTooltip = function() {
+                      tooltip_div.html("<b class='tooltip tooltitle'>" + d.key + "</b><div>"
+                         + "<table><tr><td><b># Commits</b></td><td>" + format(d.values.count) + "</td></tr>"
+                         + "<tr><td><b>Total</b></td><td>" + format(d.values.total) + "</td></tr>"
+                         + "<tr><td><b>Additions</b></td><td>" + format(d.values.additions) + "</td></tr>"
+                         + "<tr><td><b>Deletions</b></td><td>" + format(d.values.deletions) + "</td></tr></table></div>");    
+                  }
+                      putImageInTooltip();
+          })
+          .on("mouseout",function(){
+                tooltip_div.transition()      
+                    .duration(200)
+                    .style("opacity", 0);
+          });
+          // .on("click", function(){
+          //       console.log()
+          // });
+          
+  var nodeAnchorsRepo = nodeRepo.append("a")
+    .html(function(d) { return d.values.total ? (d.key =="test post please ignore" ?
+                                                       "<span style='font-size: 16px'><b>Top scoring reddit post of all time:</b></span><br> test post please ignore" :
+                                                        d.key) : null ;})
+    // .attr('href',function(d)  {return d.values.total ? d.key : null; } )
+    
+  // ---------------------------------------
+    
+  var nodelinkUser = userMapDiv.datum({values: userData, key:'user_treemap'}).selectAll(".nodelink")
+      .data(userTreeMap.nodes)
+    .enter().append("a")
+      .attr("class", "nodelink")
+      .attr('href',function(d)  {return d.values.total ? d.key : null; })
+
+  var nodeUser = nodelinkUser
+    .append("div")
+      .attr("class", "node")
+      .call(position)
+      .attr("stroke", "#FFF")
+      .style("background", function(d) { return d.values ? d3.rgb(color(d.key)) : null; })
+           .on("mouseover", function(d){
+                tooltip_div       
+                    .style("visibility", "visible")
+                    .transition().duration(150)
+                    .style("opacity", 1);
+                  var putImageInTooltip = function() {
+                      tooltip_div.html("<b class='tooltip tooltitle'>" + d.key + "</b><div>"
+                         + "<table><tr><td><b># Commits</b></td><td>" + format(d.values.count) + "</td></tr>"
+                         + "<tr><td><b>Total</b></td><td>" + format(d.values.total) + "</td></tr>"
+                         + "<tr><td><b>Additions</b></td><td>" + format(d.values.additions) + "</td></tr>"
+                         + "<tr><td><b>Deletions</b></td><td>" + format(d.values.deletions) + "</td></tr></table></div>");    
+                  }
+                      putImageInTooltip();
+          })
+          .on("mouseout",function(){
+                tooltip_div.transition()      
+                    .duration(200)
+                    .style("opacity", 0);
+          });
+          
+  var nodeAnchorsUser = nodeUser.append("a")
+    .html(function(d) { return d.values.total ? (d.key =="test post please ignore" ?
+                                                       "<span style='font-size: 16px'><b>Top scoring reddit post of all time:</b></span><br> test post please ignore" :
+                                                        d.key) : null ;})
+    .attr('href',function(d)  {return d.values.total ? d.key : null; } )
+
+  // ---------------------------------------
+
+  d3.selectAll(".mode-radio").on("change", function change() {
+    var value = this.value === "total"
+      ? function(d) { return d.values.total; }
+      : this.value === "additions"
+          ? function(d) { return d.values.additions; } 
+          : this.value === "deletions" 
+              ? function(d) { return d.values.deletions; } 
+              : function(d) { return d.values.count; };
+    nodeRepo
+      .data(repoTreeMap.value(value).nodes)
+      .transition()
+      .duration(1500)
+      .call(position);
+    nodeUser
+      .data(userTreeMap.value(value).nodes)
+      .transition()
+      .duration(1500)
+      .call(position);
+  });
+  
 }
 
 function position() {
