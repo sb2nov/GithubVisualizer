e86208277d9dcb1b02139086451ac6ba5cb6b17c
Sourabh <sb2nov@gmail.com>
2013-12-03 23:04:44 -0500

    reformat filter function

--- js/gitvis.js
+++ js/gitvis.js
@@ -223,70 +223,100 @@ function filterData(extentVals){
         filteredData = rawdata.filter(function(d) {
             return d.dateob > extentVals[0] && d.dateob < extentVals[1];
         });
+        if(filteredData.length == 0){
+            filteredData = rawdata;
+            extentVals = xScaleTimeBrush.domain();
+        }
     };
 
-    filteredTimeNest = d3.nest()
+    allChartTimeLineNest = d3.nest()
         .key(function(d) {return d.timestamp;})
         .sortKeys(compareDates)
         .rollup(rollLeaves)
         .entries(filteredData);
-    
-    filterTimeRepo = filteredData;
-    filterTimeRepoNest = null;
-    if(repoSelected) {
-        filterTimeRepo = filteredData.filter(function(d) {return d.repo == repoSelected;});
 
-        filterTimeRepoNest = d3.nest()
-            .key(function(d) {return d.timestamp;})
-            .sortKeys(compareDates)
+    repoMapDataNest = null;
+    userMapDataNest = null;
+    secondChartTimeLineNest = null;
+    thirdChartTimeLineNest = null;
+
+    if(!firstClick){
+        repoMapDataNest = d3.nest()
+            .key(function(d) {return d.repo;})
             .rollup(rollLeaves)
-            .entries(filterTimeRepo);
+            .entries(filteredData);
+            
+        userMapDataNest = d3.nest()
+            .key(function(d) {return d.username;})
+            .rollup(rollLeaves)
+            .entries(filteredData);
     }
     
-    filterRepoNest = d3.nest()
-        .key(function(d) {return d.repo;})
-        .rollup(rollLeaves)
-        .entries(filterTimeRepo);
-
-    filterTimeUser = filteredData;
-    filterTimeUserNest = null;
-    if(userSelected){
-        filterTimeUser = filteredData.filter(function(d) {return d.username == userSelected;});
+    if(firstClick=='repo'){
         
-        filterTimeUserNest = d3.nest()
+        filterFirstClickData = filteredData.filter(function(d) {return d.repo == repoSelected;});
+
+        repoMapDataNest = d3.nest()
+            .key(function(d) {return d.repo;})
+            .rollup(rollLeaves)
+            .entries(filteredData);
+
+        userMapDataNest = d3.nest()
+            .key(function(d) {return d.username;})
+            .rollup(rollLeaves)
+            .entries(filterFirstClickData);
+        
+        secondChartTimeLineNest = d3.nest()
             .key(function(d) {return d.timestamp;})
             .sortKeys(compareDates)
             .rollup(rollLeaves)
-            .entries(filterTimeUser);
+            .entries(filterFirstClickData);
+
     }
     
-    filterUserNest = d3.nest()
-        .key(function(d) {return d.username;})
-        .rollup(rollLeaves)
-        .entries(filterTimeUser);
+    if(firstClick=='user'){
 
-    filterTimeRepoUser = filterTimeUser;    
-    filterTimeRepoUserNest = null;
-    if(userSelected && repoSelected){
-        filterTimeRepoUser = filterTimeUser.filter(function(d) {return d.repo == repoSelected;});
+        filterFirstClickData = filteredData.filter(function(d) {return d.username == userSelected;});
 
-        filterTimeRepoUserNest = d3.nest()
+        repoMapDataNest = d3.nest()
+            .key(function(d) {return d.repo;})
+            .rollup(rollLeaves)
+            .entries(filterFirstClickData);
+
+        userMapDataNest = d3.nest()
+            .key(function(d) {return d.username;})
+            .rollup(rollLeaves)
+            .entries(filteredData);
+
+        secondChartTimeLineNest = d3.nest()
             .key(function(d) {return d.timestamp;})
             .sortKeys(compareDates)
             .rollup(rollLeaves)
-            .entries(filterTimeRepoUser);
+            .entries(filterFirstClickData);
+
     }
+
+    if(secondClick){
+        filterSecondClickData = filterFirstClickData.filter(function(d) { return d.username == userSelected && d.repo == repoSelected;});
+
+        thirdChartTimeLineNest = d3.nest()
+            .key(function(d) {return d.timestamp;})
+            .sortKeys(compareDates)
+            .rollup(rollLeaves)
+            .entries(filterSecondClickData);
+    }
+
+    
     
     objData = new Object();
     // TimeStamp Filters
-    objData['time'] = filteredTimeNest;
-    objData['repoTime'] = filterTimeRepoNest;
-    objData['userTime'] = filterTimeUserNest;
-    objData['userRepoTime'] = filterTimeRepoUserNest;
+    objData['all_timeline_data'] = allChartTimeLineNest;
+    objData['second_timeline_data'] = secondChartTimeLineNest;
+    objData['third_timeline_data'] = thirdChartTimeLineNest;
 
     // Treemap Data Filters
-    objData['repo'] = filterRepoNest;
-    objData['user'] = filterUserNest;
+    objData['repo_map_data'] = repoMapDataNest;
+    objData['user_map_data'] = userMapDataNest;
     
     //Heatmap Data Filters
     
@@ -300,9 +330,8 @@ function renderFunc(error, csv){
 
     // Data Manipulation
     var dataobj = filterData(null);
-    uniTimeSeries = $.extend(true, [], dataobj.time);
-    // console.log(dataobj);
-
+    uniTimeSeries = $.extend(true, [], dataobj.all_timeline_data);
+    console.log(dataobj);
     render_routine(dataobj);
 }
 
@@ -314,7 +343,7 @@ function render_routine(dataobj){
 
 function renderTimeLine(dataobj){
 
-    var timeData = dataobj.time;
+    var timeData = dataobj.all_timeline_data;
 
     // Scale Domain Set
     xScaleTimeLine.domain([new Date(timeData[0].key), d3.time.day.offset(new Date(timeData[timeData.length - 1].key), 1)]);
@@ -390,316 +419,10 @@ function renderTimeBrush() {
 
 
 function renderRepoMap(dataobj){
+    // Repomap Render Function
+    // var repoData = repoTreeMap.nodes()
     
 }
-
-
-// function formatData(error, csv) {
-
-//     timeData = d3.nest()
-//       .key(function(d) {return d.timestamp;})
-//       .sortKeys(function(a,b) {
-//           var a_date = new Date(Date.parse(a));
-//           var b_date = new Date(Date.parse(b));
-//           return a_date - b_date;
-//       })
-//       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
-//       .entries(csv);
-
-//     repoData = d3.nest()
-//       .key(function(d) {return d.repo;})
-//       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
-//       .entries(csv);
-
-//     userData = d3.nest()
-//       .key(function(d) {return d.username;})
-//       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
-//       .entries(csv);
-
-//     repoTimeData = d3.nest()
-//       .key(function(d) {return d.repo;})
-//       .key(function(d) {return d.timestamp;})
-//       .sortKeys(function(a,b) {
-//           var a_date = new Date(Date.parse(a));
-//           var b_date = new Date(Date.parse(b));
-//           return a_date - b_date;
-//       })
-//       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
-//       .entries(csv);
-
-//     repoTimeDataObj = new Object();
-//     repoTimeData.forEach(function(d) {repoTimeDataObj[d.key] = d.values;});
-
-//     userTimeData = d3.nest()
-//       .key(function(d) {return d.username;})
-//       .key(function(d) {return d.timestamp;})
-//       .sortKeys(function(a,b) {
-//           var a_date = new Date(Date.parse(a));
-//           var b_date = new Date(Date.parse(b));
-//           return a_date - b_date;
-//       })
-//       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
-//       .entries(csv);
-
-//     userTimeDataObj = new Object();
-//     userTimeData.forEach(function(d) {userTimeDataObj[d.key] = d.values;});
-
-//     userRepoData = d3.nest()
-//       .key(function(d) {return d.username;})
-//       .key(function(d) {return d.repo;})
-//       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
-//       .entries(csv);
-
-//     userRepoDataObj = new Object();
-//     userRepoData.forEach(function(d) {userRepoDataObj[d.key] = d.values;});
-
-//     repoUserData = d3.nest()
-//       .key(function(d) {return d.repo;})
-//       .key(function(d) {return d.username;})
-//       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
-//       .entries(csv);
-
-//     repoUserDataObj = new Object();
-//     repoUserData.forEach(function(d) {repoUserDataObj[d.key] = d.values;});
-      
-//     repoUserTimeData = d3.nest()
-//       .key(function(d) {return d.repo;})
-//       .key(function(d) {return d.username;})
-//       .key(function(d) {return d.timestamp;})
-//       .sortKeys(function(a,b) {
-//           var a_date = new Date(Date.parse(a));
-//           var b_date = new Date(Date.parse(b));
-//           return a_date - b_date;
-//       })
-//       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
-//       .entries(csv);
-
-//     repoUserTimeDataObj = new Object();
-//     repoUserTimeData.forEach(function(d) {
-//       tempObj = new Object();
-//       d.values.forEach(function(d) { tempObj[d.key] = d.values;})
-//       repoUserTimeDataObj[d.key] = tempObj;}
-//     );
-
-//     userRepoTimeData = d3.nest()
-//       .key(function(d) {return d.username;})
-//       .key(function(d) {return d.repo;})
-//       .key(function(d) {return d.timestamp;})
-//       .sortKeys(function(a,b) {
-//           var a_date = new Date(Date.parse(a));
-//           var b_date = new Date(Date.parse(b));
-//           return a_date - b_date;
-//       })
-//       .rollup(function(leaves) { return {"count": leaves.length, "total": d3.sum(leaves, function(d) {return parseFloat(d.total);}), "additions": d3.sum(leaves, function(d) {return parseFloat(d.additions);}), "deletions": d3.sum(leaves, function(d) {return parseFloat(d.deletions);})} })
-//       .entries(csv);
-
-//     userRepoTimeDataObj = new Object();
-//     userRepoTimeData.forEach(function(d) {
-//       tempObj = new Object();
-//       d.values.forEach(function(d) { tempObj[d.key] = d.values;})
-//       userRepoTimeDataObj[d.key] = tempObj;}
-//     );
-
-//       // Main Call
-//       draw();
-
-// }
-
-// function draw(){
-//   if(userSelected){
-//     repoDataToUse = {values: userRepoDataObj[userSelected]}
-//   }
-//   else{
-//     repoDataToUse = {values: repoData}
-//   }
-//   repoDataToUse['key'] = 'repo_treemap'
-
-//   if(repoSelected){
-//     userDataToUse = {values: repoUserDataObj[repoSelected]}
-//   }
-//   else{
-//     userDataToUse = {values: userData}
-//   }
-//   userDataToUse['key'] = 'user_treemap'
-
-//   drawTreeMap(repoDataToUse, userDataToUse);
-// }
-
-
-// function drawTreeMap(repoDataToUse, userDataToUse){
-
-//   // console.log(repoDataToUse)
-//   // console.log(repoTreeMap.nodes(repoDataToUse))
-
-//   nodelinkRepo = repoMapDiv.selectAll(".nodelink")
-//     .data(repoTreeMap.nodes(repoDataToUse))
-//     .enter()
-//       .append("a")
-//       .attr("class", "nodelink")
-//       .each(function(d) {
-//           d3.select(this)
-//             .append("div")
-//             .attr("class", "node")
-//             .each(function(d) {
-//                 d3.select(this)
-//                   .append("a")
-//                   .attr("class", "nodeanchor")
-//             });  
-//       })
-
-//   repoMapDiv.selectAll(".nodelink")
-//     .data(repoTreeMap.nodes(repoDataToUse))
-//     .exit().remove();
-
-//   nodeRepo = repoMapDiv.selectAll(".node")
-//     .data(repoTreeMap.nodes(repoDataToUse))
-//     .exit().remove()
-
-//   nodeRepo = repoMapDiv.selectAll(".node")
-//       .data(repoTreeMap.nodes(repoDataToUse))
-//       .attr("class", "node")
-//       .call(position)
-//       .attr("stroke", "#FFF")
-//       .style("background", function(d) { return d.values ? d3.rgb(color(d.key)) : null; })
-//            .on("mouseover", function(d){
-//                 tooltip_div       
-//                     .style("visibility", "visible")
-//                     .transition().duration(150)
-//                     .style("opacity", 1);
-//                   var putImageInTooltip = function() {
-//                       tooltip_div.html("<b class='tooltip tooltitle'>" + d.key + "</b><div>"
-//                          + "<table><tr><td><b># Commits</b></td><td>" + format(d.values.count) + "</td></tr>"
-//                          + "<tr><td><b>Total</b></td><td>" + format(d.values.total) + "</td></tr>"
-//                          + "<tr><td><b>Additions</b></td><td>" + format(d.values.additions) + "</td></tr>"
-//                          + "<tr><td><b>Deletions</b></td><td>" + format(d.values.deletions) + "</td></tr></table></div>");    
-//                   }
-//                       putImageInTooltip();
-//           })
-//           .on("mouseout",function(){
-//                 tooltip_div.transition()      
-//                     .duration(200)
-//                     .style("opacity", 0);
-//           })
-//           .on("click", function(d){
-//                 repoSelected = d.key;
-//                 if(userSelected==null){
-//                     draw();                  
-//                 }
-//           });
-
-//   nodeAnchorsRepo = repoMapDiv.selectAll(".nodeanchor")
-//     .data(repoTreeMap.nodes(repoDataToUse))
-//     .html(function(d) { return d.values.total ? d.key : null ;})
-//     .exit().remove();
-
-//   if(userSelected){
-//     repoMapDiv.selectAll(".nodelink")
-//       .attr('href', function(d){return d.values.total ? "https://github.com/balanced/" + d.key : null;})
-//       .attr('target', '_blank');
-//     repoMapDiv.selectAll(".nodeanchor")
-//       .attr('href', function(d){return d.values.total ? "https://github.com/balanced/" + d.key : null;})
-//       .attr('target', '_blank');
-//   }
-
-//   // ---------------------------------------
-
-//   // console.log(userDataToUse)
-//   // console.log(userTreeMap.nodes(userDataToUse))
-
-//   nodelinkUser = userMapDiv.selectAll(".nodelink")
-//     .data(userTreeMap.nodes(userDataToUse))
-//     .enter()
-//     .append("a")
-//     .attr("class", "nodelink")
-//     .each(function(d) {
-//         d3.select(this)
-//           .append("div")
-//           .attr("class", "node")
-//           .each(function(d) {
-//               d3.select(this)
-//                 .append("a")
-//                 .attr("class", "nodeanchor")
-//           });  
-//     })
-
-//   userMapDiv.selectAll(".nodelink")
-//     .data(userTreeMap.nodes(userDataToUse))
-//     .exit().remove();
-
-//   nodeUser = userMapDiv.selectAll(".node")
-//     .data(userTreeMap.nodes(userDataToUse))
-//     .exit().remove()
-
-//   nodeUser = userMapDiv.selectAll(".node")
-//       .data(userTreeMap.nodes(userDataToUse))
-//       .attr("class", "node")
-//       .call(position)
-//       .attr("stroke", "#FFF")
-//       .style("background", function(d) { return d.values ? d3.rgb(color(d.key)) : null; })
-//            .on("mouseover", function(d){
-//                 tooltip_div       
-//                     .style("visibility", "visible")
-//                     .transition().duration(150)
-//                     .style("opacity", 1);
-//                   var putImageInTooltip = function() {
-//                       tooltip_div.html("<b class='tooltip tooltitle'>" + d.key + "</b><div>"
-//                          + "<table><tr><td><b># Commits</b></td><td>" + format(d.values.count) + "</td></tr>"
-//                          + "<tr><td><b>Total</b></td><td>" + format(d.values.total) + "</td></tr>"
-//                          + "<tr><td><b>Additions</b></td><td>" + format(d.values.additions) + "</td></tr>"
-//                          + "<tr><td><b>Deletions</b></td><td>" + format(d.values.deletions) + "</td></tr></table></div>");    
-//                   }
-//                       putImageInTooltip();
-//           })
-//           .on("mouseout",function(){
-//                 tooltip_div.transition()      
-//                     .duration(200)
-//                     .style("opacity", 0);
-//           })
-//           .on("click", function(d){
-//                 userSelected = d.key;
-//                 if(repoSelected==null){
-//                   draw(); 
-//                 }
-//           });
-
-//   nodeAnchorsUser = userMapDiv.selectAll(".nodeanchor")
-//     .data(userTreeMap.nodes(userDataToUse))
-//     .html(function(d) { return d.values.total ? d.key : null ;})
-//     .exit().remove();
-
-//   if(repoSelected){
-//     userMapDiv.selectAll(".nodelink")
-//       .attr('href', function(d){return d.values.total ? "https://github.com/" + d.key : null;})
-//       .attr('target', '_blank');
-//     userMapDiv.selectAll(".nodeanchor")
-//       .attr('href', function(d){return d.values.total ? "https://github.com/" + d.key : null;})
-//       .attr('target', '_blank');
-//   }
-
-//   // ---------------------------------------
-
-//   d3.selectAll(".mode-radio").on("change", function change() {
-//     var value = this.value === "total"
-//       ? function(d) { return d.values.total; }
-//       : this.value === "additions"
-//           ? function(d) { return d.values.additions; } 
-//           : this.value === "deletions" 
-//               ? function(d) { return d.values.deletions; } 
-//               : function(d) { return d.values.count; };
-
-//     nodeRepo
-//       .data(repoTreeMap.value(value).nodes(repoDataToUse))
-//       .transition()
-//       .duration(500)
-//       .call(position);
-//     nodeUser
-//       .data(userTreeMap.value(value).nodes(userDataToUse))
-//       .transition()
-//       .duration(1500)
-//       .call(position);
-//   });
-
-// }
 
 
 function brushed(){
@@ -738,3 +461,6 @@ function timeStampDiff(dateArray){
     // days
     return Math.ceil(diffInMilliseconds / 1000 / 60 / 60 / 24);
 }
+
+// Done
+// By : Sourabh Bajaj
